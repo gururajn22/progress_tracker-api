ac0e02c1df0717b7d64aab34d572e82b9f2d0e2a
Gururaj N <127120378+gururajn22@users.noreply.github.com>
2023-12-15 20:04:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { router } from "./routes/url.js";
+
 const app = express();
 dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 app.use("/todo", router);
-const PORT= process.env.PORT || 4000;
-const mongoConn=process.env.MONGO_CONN;
-mongoose.connect(mongoConn).then(() => {
-  console.log("monogo connection working");
+
+const PORT = process.env.PORT || 4000;
+const mongoConn = process.env.MONGO_CONN;
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(mongoConn, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connection established successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the application on connection error
+  }
+};
+
+connectToDatabase().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
